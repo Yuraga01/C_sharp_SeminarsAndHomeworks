ff2e868d9880d4e97eaf174b2202241bf0c9cd8d
Akhtyrsky <ahtrskiy@gmail.com>
2023-06-24 19:48:17 +0300

    Решена первая задача

---
+++ Seminar1/SeminarTask1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*  
+Опрелеляем является ли первое введенное 
+число квадратом второго введенного числа
+*/
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (b * b == a)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+Console.WriteLine("End");
