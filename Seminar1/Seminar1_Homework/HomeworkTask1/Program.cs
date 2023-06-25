10f5a99de7983618dfdf2c4946829b0658c6e3b5
Akhtyrsky <ahtrskiy@gmail.com>
2023-06-25 09:28:11 +0300

    Первая задача решена

---
+++ Seminar1/Seminar1_Homework/HomeworkTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, 
+  какое число большее, а какое меньшее. */
+ int a = int.Parse(Console.ReadLine());
+ int b = int.Parse(Console.ReadLine());
+
+ if (a > b)
+ {
+    Console.WriteLine($"Max = {a}");
+ }
+ else
+ {
+    Console.WriteLine($"Max = {b}");
+ }
+ 
