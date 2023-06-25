321465f0fc9dfe1b804992033e5bfd029c14acba
Akhtyrsky <ahtrskiy@gmail.com>
2023-06-25 09:39:44 +0300

    Вторая задача решена

---
+++ Seminar1/Seminar1_Homework/HomeworkTask2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает 
+на вход три числа и выдаёт максимальное из этих чисел.*/
+Console.Write("Введите первое число: ");
+ int a = int.Parse(Console.ReadLine());
+ Console.Write("Введите второе число: ");
+ int b = int.Parse(Console.ReadLine());
+ Console.Write("Введите третье число: ");
+ int c = int.Parse(Console.ReadLine());
+
+ if (a>b && a>c)
+ {
+ Console.WriteLine($"Максимальное число {a}");   
+ }
+ else if (b>a && b>c)
+ {
+    Console.WriteLine($"Максимальное число {b}");
+ }
+ else 
+ Console.WriteLine($"Максимальное число {c}");
