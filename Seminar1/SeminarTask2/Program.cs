daf82a84fa5e5d40d3d69feacc062a8b3141be1f
Akhtyrsky <ahtrskiy@gmail.com>
2023-06-25 08:58:08 +0300

    Добавлен способ решения через switch/case

--- Seminar1/SeminarTask2/Program.cs
+++ Seminar1/SeminarTask2/Program.cs
@@ -1,6 +1,42 @@
 ﻿Console.Write("Введите день недели: ");
 int day = int.Parse(Console.ReadLine());
-if ((day >=1) &&  (day <= 7))
+
+switch (day)
+{
+    case 1:
+    Console.WriteLine("Понедельник");
+    break;
+    case 2:
+    Console.WriteLine("Вторник");
+    break;
+    case 3:
+    Console.WriteLine("Среда");
+    break;
+    case 4:
+    Console.WriteLine("Четверг");
+    break;
+    case 5:
+    Console.WriteLine("Пятница");
+    break;
+    case 6:
+    Console.WriteLine("Суббота");
+    break;
+    case 7:
+    Console.WriteLine("Воскресенье");
+    break;
+    default:
+    Console.WriteLine("Нет такого дня");
+    break;
+}
+
+
+
+
+
+
+
+
+/* if ((day >=1) &&  (day <= 7))
 {
     if (day == 1)
 {
@@ -35,5 +71,5 @@ else
 {
 Console.WriteLine("Такого дня недели не существует");
 }
-
+*/
 // можно было испольщовать switch case
