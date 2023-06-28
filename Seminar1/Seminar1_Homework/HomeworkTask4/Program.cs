2c4fbfd0b9abf0e74b48657a79700102da51f0f4
Akhtyrsky <ahtrskiy@gmail.com>
2023-06-28 23:36:09 +0300

    решение четвертой задачи

---
+++ Seminar1/Seminar1_Homework/HomeworkTask4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter the number: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    if (i%2==0)
+        {
+            Console.Write(i);
+            Console.Write(" ");
+        }
+}
