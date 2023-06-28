a7256d4cc7ee9466085ad2eb85d42a6d8f7d21b3
Akhtyrsky <ahtrskiy@gmail.com>
2023-06-28 10:30:29 +0300

    Решение третьей задачи

---
+++ Seminar1/Seminar1_Homework/Homeworktask3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter the number: ");
+ int a = int.Parse(Console.ReadLine());
+ Console.WriteLine("Variable is even-numbered? ");
+ if (a % 2 == 0)
+ {
+    Console.Write("Yes");
+ }
+ else
+ {
+   Console.Write("No");
+ }
