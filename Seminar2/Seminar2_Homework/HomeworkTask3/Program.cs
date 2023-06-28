c812384fc33945065c8b891d4e4bb063b5e54847
Akhtyrsky <ahtrskiy@gmail.com>
2023-06-29 00:12:22 +0300

    Решение третьей задачи

---
+++ Seminar2/Seminar2_Homework/HomeworkTask3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите номер дня недели: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Это выходной?");
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("Да");
+}
+else if (a >= 1 && a <= 5)
+{
+    Console.WriteLine("Нет");
+}
+else
+{
+    Console.WriteLine("Нет такого дня");
+}
