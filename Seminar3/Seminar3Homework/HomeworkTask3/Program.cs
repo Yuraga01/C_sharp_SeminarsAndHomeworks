084e737df965d9fc3859dacd0e75ed38932a3595
Akhtyrsky <ahtrskiy@gmail.com>
2023-07-06 00:54:43 +0300

    Решение третьей задачи

---
+++ Seminar3/Seminar3Homework/HomeworkTask3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+//от 1 до N.
+
+
+void PrintCubTab(int N){
+    if (N < 1) {
+        Console.WriteLine("There are no valid numbers");
+    }else{
+        Console.Write($"{N} --> ");
+        for (int i = 1; i <= N; i++){            
+            Console.Write($"{Math.Pow(i,3)}; ");
+        }
+        Console.WriteLine(" ");
+    }  
+}
+
+//-------------------------
+
+Console.Write("Please n: ");
+int num = int.Parse(Console.ReadLine());
+PrintCubTab(num);
+
