66f9a838213e3f73a6095962f40a6f070abe8468
Akhtyrsky <ahtrskiy@gmail.com>
2023-07-06 01:05:03 +0300

    Решение первой задачи

---
+++ Seminar3/Seminar3Homework/Homeworktask1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите пятизначное число : ");
+string text = Console.ReadLine();
+char[] obrtext = text.ToCharArray();
+Array.Reverse(obrtext);
+string finaltext = new string (obrtext);
+Console.WriteLine("Является ли число палиндромом?");
+       if (text == finaltext)
+       {
+         Console.WriteLine("Да");
+       }
+       if (text != finaltext)
+       {
+         Console.WriteLine("Нет");
+       }
+     
