dc560a49f0bbef20eda6e2d7b84945c50f9077f5
Akhtyrsky <ahtrskiy@gmail.com>
2023-07-07 11:43:29 +0300

    Первая семинарская задача

---
+++ Seminar4/SeminarTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿void Kol (int n)
+{
+    int kl;
+for (kl = 0; n > 0; kl++)
+{
+    n=n/10;
+   }
+Console.WriteLine(kl);
+}
+
+Console.WriteLine("Введите ваше число: ");
+int N = int.Parse(Console.ReadLine());
+Kol(N);
+
+//int kl = 0, a = N;
+
+/*while (a>0)
+{
+    a=a/10;
+    kl++;
+}
+Console.WriteLine(kl);
+*/
+
