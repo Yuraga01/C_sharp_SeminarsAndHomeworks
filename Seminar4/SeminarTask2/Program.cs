12f125cb3d621b0da2e38ab462a5e4fee7cd9974
Akhtyrsky <ahtrskiy@gmail.com>
2023-07-07 11:57:01 +0300

    Вторая семинарская задача

---
+++ Seminar4/SeminarTask2/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход число N, а выдает произведение числе от 1 до N
+4 --> 24
+5 --> 120 */
+
+int multiply (int numUs)
+{
+    int temp = 1;
+    for (int i = 1; i <= numUs; i++)
+    {
+temp = temp *i;
+    }
+return temp;
+}
+Console.Write("Enter the number: ");
+int numUs = int.Parse(Console.ReadLine());
+int result = (multiply(numUs));
+Console.WriteLine(result);
