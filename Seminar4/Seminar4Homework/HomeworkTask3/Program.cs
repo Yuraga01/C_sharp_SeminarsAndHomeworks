a1cafcf4272f371407d677df5d824c8f8b83bbab
Akhtyrsky <ahtrskiy@gmail.com>
2023-07-08 22:43:41 +0300

    Решение третьей задачи ДЗ

---
+++ Seminar4/Seminar4Homework/HomeworkTask3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] Array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    Array[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", Array));
+
+    
