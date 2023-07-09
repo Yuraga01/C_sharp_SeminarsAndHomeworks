51e6a653b3fb0aa12e5fd342e1c72f0ac1e047e2
Akhtyrsky <ahtrskiy@gmail.com>
2023-07-09 11:31:39 +0300

    Решение второй задачи ДЗ

---
+++ Seminar4/Seminar4Homework/HomeworkTask2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+
+int SumAllDigit (int number)
+{
+int result = 0;
+while (number >0)
+{
+    result += number %10;
+    number = number /10;
+}
+return result;
+}
+Console.WriteLine ("Enter number: ");
+int number = int.Parse (Console.ReadLine ());
+Console.WriteLine(SumAllDigit(number));
