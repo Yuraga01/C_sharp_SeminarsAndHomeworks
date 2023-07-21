f89c291c144c1c7da58ba1cff4edb4db980f2e6f
Akhtyrsky <ahtrskiy@gmail.com>
2023-07-21 22:46:19 +0300

    Решение третьей задачи ДЗ

---
+++ Seminar7/Homework/HWTask3/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+double [] GetMean(int[,] array)
+{
+    double[] resultArray = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sumColumn = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sumColumn = sumColumn + array[j, i];
+        }
+        resultArray[i] = sumColumn / array.GetLength(0);
+    }
+    return resultArray;
+}
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] array = CreateArray(rows, columns);
+PrintArray(array);
+Console.WriteLine();
+string str = string.Join(", ", GetMean(array));
+Console.WriteLine(str);
